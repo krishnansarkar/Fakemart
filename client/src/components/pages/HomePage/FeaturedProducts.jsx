663eb75862e8989ce2d5d8598886c7643aa88854
Krishnan Sarkar <sarkar.krishnan@gmail.com>
2024-03-15 22:16:35 -0400

    Fix image sizes in FeaturedProducts to be aligned

--- client/src/components/pages/HomePage/FeaturedProducts.jsx
+++ client/src/components/pages/HomePage/FeaturedProducts.jsx
@@ -1,10 +1,3 @@
-//Todo:
-// - I -
-// 1. Fix final columns being different sizes if less than 3 left.
-// 2. Test different image sizes.
-// 3. Fix any size differentiation of images.
-// - II -
-// Fix color of background to off-white.
 import { Container, Card, ListGroup, Row, Col } from "react-bootstrap";
 
 export default function FeaturedProducts() {
@@ -14,24 +7,28 @@ export default function FeaturedProducts() {
       quantity: "5 Kg",
       price: "$39.99",
       deal: "$29.99",
+      image: process.env.PUBLIC_URL + "/images/logo.png",
     },
     {
       name: "PMO Soft Tofu",
       quantity: "16 Oz",
       price: "$2.49",
       deal: "3 for $6",
+      image: process.env.PUBLIC_URL + "/images/KoreanFoodSaleFlyer.png",
     },
     {
       name: "3",
       quantity: "16 Oz",
       price: "$2.49",
       deal: "3 for $6",
+      image: process.env.PUBLIC_URL + "/images/logo.png",
     },
     {
       name: "4",
       quantity: "16 Oz",
       price: "$2.49",
       deal: "3 for $6",
+      image: process.env.PUBLIC_URL + "/images/logo.png",
     },
   ];
 
@@ -42,31 +39,41 @@ export default function FeaturedProducts() {
     }
   );
 
+  const cardImageStyle = {
+    height: "180px",
+    "object-fit": "cover",
+  };
+
   return (
-    <Container className="bg-white py-5" id="featureddeals">
-      <h2 className="text-center">Featured Products and Deals</h2>
-      {groupedProducts.map((group, r) => (
-        <Row key={r} className="mb-4">
-          {group.map((product, c) => (
-            <Col key={r + c}>
-              <Card>
-                <Card.Img
-                  variant="top"
-                  src={process.env.PUBLIC_URL + "/images/logo.png"}
-                />
-                <ListGroup className="list-group-flush">
-                  <ListGroup.Item>{product.name}</ListGroup.Item>
-                  <ListGroup.Item>Quantity: {product.quantity}</ListGroup.Item>
-                  <ListGroup.Item>Original: {product.price}</ListGroup.Item>
-                  <ListGroup.Item>Sale: {product.deal}</ListGroup.Item>
-                </ListGroup>
-              </Card>
-            </Col>
-          ))}
-          {group.length < 3 && <Col></Col>}
-          {group.length < 2 && <Col></Col>}
-        </Row>
-      ))}
+    <Container fluid className="bg-white py-5" id="featureddeals">
+      <h2 className="text-center mb-3">Featured Products and Deals</h2>
+      <div className="px-5">
+        {groupedProducts.map((group, r) => (
+          <Row key={r}>
+            {group.map((product, c) => (
+              <Col key={r + c}>
+                <Card className="mb-4">
+                  <Card.Img
+                    variant="top"
+                    src={product.image}
+                    style={cardImageStyle}
+                  />
+                  <ListGroup className="list-group-flush">
+                    <ListGroup.Item>{product.name}</ListGroup.Item>
+                    <ListGroup.Item>
+                      Quantity: {product.quantity}
+                    </ListGroup.Item>
+                    <ListGroup.Item>Original: {product.price}</ListGroup.Item>
+                    <ListGroup.Item>Sale: {product.deal}</ListGroup.Item>
+                  </ListGroup>
+                </Card>
+              </Col>
+            ))}
+            {group.length < 3 && <Col></Col>}
+            {group.length < 2 && <Col></Col>}
+          </Row>
+        ))}
+      </div>
     </Container>
   );
 }
