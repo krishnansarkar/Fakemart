72434e4397d6105c9240e26ac35caf9cc1640a7d
Krishnan Sarkar <sarkar.krishnan@gmail.com>
2024-03-16 15:04:04 -0400

    Make FeaturedDisplay responsive

--- client/src/components/pages/HomePage/FeaturedDisplay.jsx
+++ client/src/components/pages/HomePage/FeaturedDisplay.jsx
@@ -9,28 +9,35 @@ export default function FeaturedDisplay() {
     "max-width": "332px",
   };
 
+  const imageStyle = {
+    maxWidth: "100%",
+  };
+
   return (
-    <Container
-      fluid
-      className="bg-primary py-5 d-flex flex-row justify-content-center"
-    >
-      <div>
-        <img
-          src={process.env.PUBLIC_URL + "/images/KoreanFoodSaleFlyer.png"}
-          alt="50% Discount on Catering food from 6PM to close."
-          width={686}
-        />
-      </div>
-      <div style={featuredLinkStyle} className="bg-white mx-4 p-5">
-        <h1>Sales going on right now.</h1>
-        <p>
-          See just a few of our sales and featured products here at Woori Mart
-          Princeton.
-        </p>
-        <Button variant="outline-secondary" href="#featureddeals">
-          Featured Deals
-        </Button>
-      </div>
+    <Container fluid className="bg-primary py-5 justify-content-center">
+      <Row>
+        <Col xs={12} md={8}>
+          <div>
+            <img
+              src={process.env.PUBLIC_URL + "/images/KoreanFoodSaleFlyer.png"}
+              alt="50% Discount on Catering food from 6PM to close."
+              style={imageStyle}
+            />
+          </div>
+        </Col>
+        <Col>
+          <div className="bg-white py-5 px-5">
+            <h1>Sales going on right now.</h1>
+            <p>
+              See just a few of our sales and featured products here at Woori
+              Mart Princeton.
+            </p>
+            <Button variant="outline-secondary" href="#featureddeals">
+              Featured Deals
+            </Button>
+          </div>
+        </Col>
+      </Row>
     </Container>
   );
 }
