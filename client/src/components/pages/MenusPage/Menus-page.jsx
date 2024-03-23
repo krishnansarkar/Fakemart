8edcba31ec1ff62dc2b4cb3cfb2d99ac43ccc4fa
Krishnan Sarkar <sarkar.krishnan@gmail.com>
2024-03-22 21:15:43 -0400

    Add details to sushi tab

--- client/src/components/pages/MenusPage/Menus-page.jsx
+++ client/src/components/pages/MenusPage/Menus-page.jsx
@@ -2,77 +2,133 @@ import { Container, Row, Col, Tab, Nav } from "react-bootstrap";
 import MenuTab from "./MenuTab";
 
 export default function MenusPage() {
-  const leftColCategories = [
-    {
-      name: "Kimbab 김밥",
-      description: "Korean style seaweed rolls",
-      items: [
-        {
-          name: "Bulgogi Beef Kimbab 불고기 김밥",
-          description: "",
-          price: "8",
-        },
-        {
-          name: "K Style Kimbab 김밥",
-          description: "Vegetable seaweed roll",
-          price: "7",
-        },
-      ],
-    },
-    {
-      name: "Lunch Boxes and Bowls 도시락",
-      items: [
-        {
-          name: "Spicy Pork Lunch Box 제육볶음",
-          description: "",
-          price: "13",
-        },
-        {
-          name: "Donkatsu Lunch Box 돈까스",
-          description: "",
-          price: "13",
-        },
-      ],
-    },
-    {
-      name: "Appetizers 분식",
-      items: [
-        {
-          name: "Pan Fried Tofu 두부튀김",
-          description: "",
-          price: "6",
-        },
-        {
-          name: "Ddukbokki 떡볶이",
-          description: "Korean style rice cakes",
-          price: "7",
-        },
-      ],
-    },
-  ];
+  const kdishcategories = {
+    leftCol: [
+      {
+        name: "Kimbab 김밥",
+        description: "Korean style seaweed rolls",
+        items: [
+          {
+            name: "Bulgogi Beef Kimbab 불고기 김밥",
+            description: "",
+            price: "8",
+          },
+          {
+            name: "K Style Kimbab 김밥",
+            description: "Vegetable seaweed roll",
+            price: "7",
+          },
+        ],
+      },
+      {
+        name: "Lunch Boxes and Bowls 도시락",
+        items: [
+          {
+            name: "Spicy Pork Lunch Box 제육볶음",
+            description: "",
+            price: "13",
+          },
+          {
+            name: "Donkatsu Lunch Box 돈까스",
+            description: "",
+            price: "13",
+          },
+        ],
+      },
+      {
+        name: "Appetizers 분식",
+        items: [
+          {
+            name: "Pan Fried Tofu 두부튀김",
+            description: "",
+            price: "6",
+          },
+          {
+            name: "Ddukbokki 떡볶이",
+            description: "Korean style rice cakes",
+            price: "7",
+          },
+        ],
+      },
+    ],
+    rightCol: [
+      {
+        name: "Rice and Noodles 밥류 면류",
+        items: [
+          {
+            name: "Shrimp Fried Rice 새우 볶음밥",
+            description: "",
+            price: "10",
+          },
+        ],
+      },
+      {
+        name: "Korean Stews 찌게류",
+        items: [
+          {
+            name: "Kimchi Stew 김치찌게",
+            description: "",
+            price: "14",
+          },
+        ],
+      },
+    ],
+  };
 
-  const rightColCategories = [
-    {
-      name: "Rice and Noodles 밥류 면류",
-      items: [
-        {
-          name: "Shrimp Fried Rice 새우 볶음밥",
-          description: "",
-          price: "10",
-        },
-      ],
-    },
-    {
-      name: "Korean Stews 찌게류",
-      items: [
-        {
-          name: "Kimchi Stew 김치찌게",
-          description: "",
-          price: "14",
-        },
-      ],
-    },
-  ];
+  const shushicategories = {
+    leftCol: [
+      {
+        name: "Rolls",
+        description: "",
+        items: [
+          {
+            name: "House Roll",
+            description:
+              "Five of each of our most popular rolls: Tuna, Salmon and California.",
+            price: "13.50",
+          },
+          {
+            name: "Tri Volcano Roll",
+            description: "",
+            price: "15",
+          },
+        ],
+      },
+    ],
+    rightCol: [
+      {
+        name: "Sushi and Sashimi",
+        items: [
+          {
+            name: "Tuna Sashimi",
+            description: "",
+            price: "10",
+          },
+        ],
+      },
+      {
+        name: "Meals and Combos",
+        items: [
+          {
+            name: "Hwedupbab",
+            description:
+              "Raw fish and assorted vegetables served over rice. Korean style.",
+            price: "16",
+          },
+        ],
+      },
+      {
+        name: "Appetizer",
+        items: [
+          {
+            name: "Inari (Seasoned Tofu)",
+            description: "",
+            price: "10.50",
+          },
+        ],
+      },
+    ],
+  };
 
   const tabContainerStyle = {
     width: "980px",
@@ -93,12 +149,17 @@ export default function MenusPage() {
             <MenuTab
               name="K-Dish"
               description="Bring Korea home with you"
-              leftColCategories={leftColCategories}
-              rightColCategories={rightColCategories}
+              leftColCategories={kdishcategories.leftCol}
+              rightColCategories={kdishcategories.rightCol}
             />
           </Tab.Pane>
           <Tab.Pane eventKey="second">
-            <h1>2nd tab</h1>
+            <MenuTab
+              name="Sushi"
+              description="Fresh sushi every day from Sushi Avocado"
+              leftColCategories={shushicategories.leftCol}
+              rightColCategories={shushicategories.rightCol}
+            />
           </Tab.Pane>
         </Tab.Content>
       </Tab.Container>
