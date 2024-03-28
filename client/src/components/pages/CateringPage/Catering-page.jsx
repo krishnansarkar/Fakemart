5582095a50728195752cc078b7d73773e65ef097
Krishnan Sarkar <sarkar.krishnan@gmail.com>
2024-03-27 22:43:13 -0400

    Extract TopDisplay from Catering-page and introduce next segment underneath

--- client/src/components/pages/CateringPage/Catering-page.jsx
+++ client/src/components/pages/CateringPage/Catering-page.jsx
@@ -1,44 +1,29 @@
-import { Container, Row, Col, Button } from "react-bootstrap";
-import { Link } from "react-router-dom";
+import { Container, Row, Col } from "react-bootstrap";
+import TopDisplay from "./TopDisplay";
 
 export default function CateringPage() {
-  const imageStyle = {
-    borderRadius: "50% 50% 0% 0%",
-    width: "455px",
-  };
-
   return (
-    <Container fluid className="bg-white py-5 justify-content-center">
-      <Row>
-        <Col>
-          <div>
+    <>
+      <TopDisplay />
+      <Container fluid className="bg-white py-5 justify-content-center">
+        <Row>
+          <Col>
+            <h1>01</h1>
+            <h3>Sushi Platters</h3>
+            <p className="lead">
+              Choose between rolls, sushi, sashimi or any combination of the
+              three. We'll work with your price point to have a sushi platter
+              for your next party or gathering.
+            </p>
+          </Col>
+          <Col>
             <img
-              src={process.env.PUBLIC_URL + "/images/bimbop.png"}
-              alt="Bimbop"
-              style={imageStyle}
+              src={process.env.PUBLIC_URL + "/images/sushiplatter.png"}
+              alt="Sushi Platter"
             />
-          </div>
-        </Col>
-        <Col>
-          <h1>Let us cater your next event</h1>
-          <p className="lead mb-3">
-            We pride ourselves on not only having the freshest ingredients, but
-            also having the best chefs in the area bringing traditional Korean
-            flavors to Woori Mart Princeton.
-          </p>
-          <p className="lead mb-3">
-            Every week we'll have a new specialty menu for catering. If you have
-            any specialty requests or questions on pricing, call or email us and
-            we will find a catering solution that works for you.
-          </p>
-          <p className="lead mb-3">
-            Free delivery on orders over $300 within 30 minutes of our store.
-          </p>
-          <Link to="/ContactUs">
-            <Button variant="outline-secondary">Contact Us</Button>
-          </Link>
-        </Col>
-      </Row>
-    </Container>
+          </Col>
+        </Row>
+      </Container>
+    </>
   );
 }
