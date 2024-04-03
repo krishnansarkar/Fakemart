1028b6ccecec6827746fa3efdd0e85afa7e7bf5f
Krishnan Sarkar <sarkar.krishnan@gmail.com>
2024-04-02 22:14:31 -0400

    Add basic cart display

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,9 +1,12 @@
 import { Link } from "react-router-dom";
-import Navbar from "react-bootstrap/Navbar";
-import Nav from "react-bootstrap/Nav";
-import Container from "react-bootstrap/Container";
+import { useState } from "react";
+import { Button, Navbar, Nav, Container, Offcanvas } from "react-bootstrap";
 
 export default function MyNavBar() {
+  const [showCart, setShowCart] = useState(false);
+  const handleCartClose = () => setShowCart(false);
+  const handleCartOpen = () => setShowCart(true);
+
   return (
     <div>
       <Navbar className="bg-primary" expand="md" collapseOnSelect>
@@ -23,6 +26,9 @@ export default function MyNavBar() {
               </Nav.Link>
             </Nav>
             <Nav classname="ml-auto">
+              <Button variant="primary" onClick={handleCartOpen}>
+                Cart
+              </Button>
               <Nav.Link as={Link} to="/ContactUs">
                 Contact Us
               </Nav.Link>
@@ -30,6 +36,12 @@ export default function MyNavBar() {
           </Navbar.Collapse>
         </Container>
       </Navbar>
+      <Offcanvas show={showCart} onHide={handleCartClose}>
+        <Offcanvas.Header closeButton>
+          <Offcanvas.Title>Cart</Offcanvas.Title>
+        </Offcanvas.Header>
+        <Offcanvas.Body>Cart items</Offcanvas.Body>
+      </Offcanvas>
     </div>
   );
 }
